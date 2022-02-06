c1e22ac61478d015ff31a37dc060b2ce0f4c9b21
George <georgekpai@gmail.com>
2022-02-06 14:22:20 +0000

    Create UserDTO and TodoDTO

---
+++ src/main/java/com/qa/todoapp/dtos/TodoDTO.java
@@ -0,0 +1,54 @@
+package com.qa.todoapp.dtos;
+
+import java.time.LocalDate;
+
+public class TodoDTO {
+	private long id;
+	private String description;
+	private LocalDate dateAdded;
+	private LocalDate dateDue;
+	private boolean complete;
+	
+	public TodoDTO() {}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getDateAdded() {
+		return dateAdded;
+	}
+
+	public void setDateAdded(LocalDate dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+
+	public LocalDate getDateDue() {
+		return dateDue;
+	}
+
+	public void setDateDue(LocalDate dateDue) {
+		this.dateDue = dateDue;
+	}
+
+	public boolean isComplete() {
+		return complete;
+	}
+
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+	
+}
