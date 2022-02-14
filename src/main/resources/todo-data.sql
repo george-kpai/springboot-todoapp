1dff77258fda832e2d9d1190b48fc85a5096ae07
George <georgekpai@gmail.com>
2022-02-14 03:39:54 +0000

    Update todo-data.sql

--- src/main/resources/todo-data.sql
+++ src/main/resources/todo-data.sql
@@ -1,4 +1,4 @@
 INSERT INTO `Todo` (`description`, `date_added`, `date_due`, `complete`, `user_id`) 
 	VALUES ('Task 1', '2020-1-1', '2020-2-2', false, 1);
 INSERT INTO `Todo` (`description`, `date_added`, `date_due`, `complete`, `user_id`) 
-	VALUES ('Task 2', '2020-5-5', '2020-6-6', false, 1);
+	VALUES ('Task 2', '2020-5-5', '2020-6-6', true, 1);
